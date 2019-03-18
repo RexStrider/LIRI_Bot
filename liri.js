fca742054bc38a9bdb2871411233fd2929886920
Odysseus <arwaterman@gmail.com>
2019-03-18 16:30:38 -0700

    added LIRI feature for concert-this command

--- liri.js
+++ liri.js
@@ -4,30 +4,49 @@ let keys = require("./keys.js");
 
 let axios = require("axios");
 
-
-
 // get command
 let command = process.argv[2];
 
-// get name of concert/song/movie
-let name = process.argv[3];
+// get a concert/song/movie
+let type = process.argv[3];
 
 // commands to implement
    // `concert-this`
+   if (command == "concert-this") {
+	   axios.get("https://rest.bandsintown.com/artists/" + type + "/events?app_id=codingbootcamp")
+	   .then( response => {
+			// console.log(response.data);
+			
+			let data=response.data;
+			for(i=0; i < data.length; i++) {
+				console.log("---------------------------------");
+				// console.log(data[i].lineup);
+				let lineup = "";
+				for(j=0; j < data[i].lineup.length; j++) {
+					if (j == (data[i].lineup.length - 1)) lineup += data[i].lineup[j];
+					else lineup += data[i].lineup[j] + " ";
+				}
+				console.log(lineup);
+				let venue = data[i].venue;
+				console.log(venue.name + ", " + venue.city + ", " + venue.region + ", " + venue.country);
+				console.log(data.datetime);
+			}
+			console.log("---------------------------------");
+	   })
+   }
    // `spotify-this-song`
-   if (process.argv[2] == "spotify-this-song") {
-	   let query = "";
-	   spotifyThisSong(process.argv[3], query);
+   if (command == "spotify-this-song") {
+	   let query = "https://api.spotify.com";
+	   spotifyThisSong(type, query);
    }
    // `movie-this`   
-   if (process.argv[2] === "movie-this") {
+   if (command === "movie-this") {
 	   // We then run the request with axios module on a URL with a JSON
-	axios.get("http://www.omdbapi.com/?t=" + process.argv[3] + "&y=&plot=short&apikey=trilogy").then(
-		function(response) {
-			// Then we print out the imdbRating
-			console.log("The movie's rating is: " + response.data.imdbRating);
-		}
-	);
+		axios.get("http://www.omdbapi.com/?t=" + type + "&y=&plot=short&apikey=trilogy")
+		.then( response => {
+				console.log(response);
+			}
+		);
    }
    // `do-what-it-says`
 
