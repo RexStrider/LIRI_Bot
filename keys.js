f1ef7bd35c3ed1c7c649e186e80128ed81e63765
Odysseus <arwaterman@gmail.com>
2019-03-18 15:34:49 -0700

    updating LIRI Bot

--- keys.js
+++ keys.js
@@ -1,6 +1,12 @@
-console.log("this is loaded!");
+console.log("keys are loaded!");
 
 exports.spotify = {
 	id: process.env.SPOTIFY_ID,
 	secret: process.env.SPOTIFY_SECRET
 };
+
+exports.dotEnv = {
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	pass: process.env.DB_PASS
+}
--- liri.js
+++ liri.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 
 let keys = require("./keys.js");
 
-let spotify = new Spotify(keys.spotify);
+let axios = require("axios");
+
+
 
 // get command
 let command = process.argv[2];
@@ -13,7 +15,35 @@ let name = process.argv[3];
 // commands to implement
    // `concert-this`
    // `spotify-this-song`
+   if (process.argv[2] == "spotify-this-song") {
+	   let query = "";
+	   spotifyThisSong(process.argv[3], query);
+   }
    // `movie-this`   
+   if (process.argv[2] === "movie-this") {
+	   // We then run the request with axios module on a URL with a JSON
+	axios.get("http://www.omdbapi.com/?t=" + process.argv[3] + "&y=&plot=short&apikey=trilogy").then(
+		function(response) {
+			// Then we print out the imdbRating
+			console.log("The movie's rating is: " + response.data.imdbRating);
+		}
+	);
+   }
    // `do-what-it-says`
 
+function spotifyThisSong(type, query) {
+	let Spotify = require('node-spotify-api');
 
+	let spotify = new Spotify(keys.spotify);
+	
+	spotify.search({
+		type: type,
+		query: query
+	}, function(error, data) {
+		if (error) {
+			return console.log('Error occurred: ' + error);
+		}
+	
+		console.log(data); 
+	});
+}
